471dbad0be1337d6ce471325ed3be16c3da5a017
Ivan Dronek <nomi29251@gmail.com>
2022-10-19 12:02:58 +0300

    Laboratory number 3 C sharp

---
+++ Laboratory_C_sharp_3/Class_of_Person.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Security.Cryptography;
+
+class Person
+{
+    private string name;
+    private int birthYear;
+    private DateTime date = DateTime.Today;
+
+    public string Name
+    {
+        get { return name; }
+    }
+
+    
+    public int BirthYear
+    {
+        get { return birthYear; }
+        set
+        {
+            if (date.Year - birthYear < 16)
+            {
+                this.name = "Very Young";
+            }
+            else
+            {
+                this.name = this.name;
+            }
+        }
+    }
+
+    
+    public Person() : this("John", 2003)
+    {
+    }
+
+    
+    public Person(string name, int birthYear)
+    {
+        this.name = name;
+        this.birthYear = birthYear;
+    }
+
+    
+    public void Age()
+    {
+        birthYear = date.Year - birthYear;
+        Console.WriteLine(birthYear);
+    }
+
+    
+    public void Input()
+    {
+        Console.OutputEncoding = System.Text.Encoding.Unicode;
+        Console.InputEncoding = System.Text.Encoding.Unicode;
+
+        Console.WriteLine("Увести ім'я:");
+        this.name = Console.ReadLine();
+        Console.WriteLine("Дата народження:");
+        this.birthYear = Convert.ToInt32(Console.ReadLine());
+    }
+
+    
+    public void ChangeName(string newName)
+    {
+        Console.OutputEncoding = System.Text.Encoding.Unicode;
+        Console.InputEncoding = System.Text.Encoding.Unicode;
+
+        this.name = newName;
+        Console.WriteLine($"Ваше ім'я  змінилось на: {newName}");
+    }
+
+    
+    public void Output()
+    {
+        Console.OutputEncoding = System.Text.Encoding.Unicode;
+        Console.InputEncoding = System.Text.Encoding.Unicode;
+
+        Console.WriteLine($"Ім'я: {this.name}, Вік: {this.birthYear}");
+    }
+
+    
+    public override string ToString()
+    {
+        return $"Ім'я: {this.name}, Вік: {this.birthYear}";
+    }
+
+    
+    public static bool operator ==(Person p1, Person p2)
+    {
+        return (p1.name == p2.name);
+    }
+    public static bool operator !=(Person p1, Person p2)
+    {
+        return (p1.name != p2.name);
+    }
+}
--- Laboratory_C_sharp_3/Program.cs
+++ Laboratory_C_sharp_3/Program.cs
@@ -1,15 +1,76 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Runtime.ConstrainedExecution;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Laboratory_C_sharp_3
+namespace Lab3_C_Sharp
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            Console.InputEncoding = System.Text.Encoding.Unicode;
+
+            Console.WriteLine("Натисніть будь-яку кнопку для щоб побачити дані про Car");
+            Console.ReadKey();
+
+            //Cars
+            Car car1 = new Car("Skoda", "Octavia A5", "Black", 12500);
+            Car car2 = new Car("Renault", "Alkana", "Red", 22000);
+            Car car3 = new Car("KIA", "SPORTAGE FL", "White", 33250);
+
+            car1.Print();
+            car1.ChangePrice(10);
+            car1.Print();
+
+            car2.Print();
+            car2.ChangePrice(10);
+            car2.Print();
+
+            car3.Print();
+            car3.ChangePrice(10);
+            car3.Print();
+
+            Console.WriteLine(car1 == car2);
+            Console.WriteLine(car2 == car3);
+
+            Console.WriteLine("Натисніть будь-яку кнопку щоб побачити дані про Person");
+            Console.ReadKey();
+
+            //Person
+            Person p1 = new Person("Giacomo", 2003);
+            Person p2 = new Person("Ben", 1999);
+            Person p3 = new Person("Milana", 1994);
+            Person p4 = new Person("Tom", 2006);
+            Person p5 = new Person("Kate", 2001);
+            Person p6 = new Person("Robert", 1997);
+
+            p1.Age();
+            p2.Age();
+            p3.Age();
+            p4.Age();
+            p5.Age();
+            p6.Age();
+
+            p1.Output();
+            p2.Output();
+            p3.Output();
+            p4.Output();
+            p5.Output();
+            p6.Output();
+
+            Console.WriteLine(p1 == p6);
+            Console.WriteLine(p4 == p6);
+
+            p2.ChangeName("Ivan");
+            p2.Output();
+
+            Console.WriteLine("Натисніть будь-яку кнопку щоб закрити консоль");
+            Console.ReadKey();
         }
     }
 }
